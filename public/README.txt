365a441526e1890424c94c4d863b0a87b20f87fd
GoldenBoy <111286150+GideonWill@users.noreply.github.com>
2025-10-08 17:38:15 +0000

    Initial commit: Demargo Interior Contractors website

---
+++ public/README.txt
@@ -0,0 +1,5 @@
+Place your assets (interior.mp4, logo.png, fabrics images) into the /public/assets folder. Example:
+
+/public/assets/interior.mp4
+/public/assets/logo.png
+/public/assets/fabrics/fabric1.jpg
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ['./index.html','./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'demargo-orange': '#ff7a00',
+        'demargo-blue': '#0b63d6'
+      }
+    }
+  },
+  plugins: [],
+}
